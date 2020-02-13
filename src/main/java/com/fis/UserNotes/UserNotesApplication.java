3b24e243211b490c7149857a97cf201374e53b1f
Akhil Garg <Akhil.Garg@fisglobal.com>
2020-02-13 23:25:41 +0530

    created spring boot usernotes application using https://start.spring.io/

---
+++ src/main/java/com/fis/UserNotes/UserNotesApplication.java
@@ -0,0 +1,13 @@
+package com.fis.UserNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fis/UserNotes/UserNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.UserNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
